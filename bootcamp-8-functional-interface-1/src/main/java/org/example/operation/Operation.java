c2daab98b22f5701d3d29bca8b9b91e2b5eadffb
sara nazemi <nazemi.sr@gmail.com>
2023-09-11 14:38:21 +0330

    add first body

---
+++ bootcamp-8-functional-interface-1/src/main/java/org/example/operation/Operation.java
@@ -0,0 +1,16 @@
+package org.example.operation;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Operation {
+    Scanner in = new Scanner(System.in);
+
+    public void splitString(StringOperation str) {
+
+        System.out.println("enter sentence :");
+        String s = in.nextLine();
+        System.out.println("hello" + str.performOperation(s));
+    }
+}
+
---
+++ bootcamp-8-functional-interface-1/src/main/java/org/example/operation/StringOperation.java
@@ -0,0 +1,7 @@
+package org.example.operation;
+
+@FunctionalInterface
+public interface StringOperation {
+
+    String performOperation(String str);
+}
